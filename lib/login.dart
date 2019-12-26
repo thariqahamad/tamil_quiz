74f265fdf8242eb50b4bb1c4a7d9ed9ce8a30e34
thariqahamad <ahamadthariq@gmail.com>
2019-12-26 21:05:30 +0530

    vfhgbfgh

--- lib/login.dart
+++ lib/login.dart
@@ -8,279 +8,91 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter custom clipper example"),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(20.0),
-        children: <Widget>[
-          ClipPath(
-            clipper: OvalBottomBorderClipper(),
+      //  resizeToAvoidBottomPadding : false,
+      // appBar: AppBar(
+      //   title: Text("Flutter custom clipper example"),
+      // ),
+      body:          Stack(
+          children: <Widget>[
+            ClipPath(
+               clipper: OvalBottomBorderClipper(),
             child: Container(
-              height: 100,
+              height:MediaQuery.of(context).size.height/2,
               color: Colors.blue,
-              child: Center(child: Text("OvalBottomBorderClipper()")),
             ),
-          ),
-          const SizedBox(height: 10.0),
-          ClipPath(
-            clipper: OvalRightBorderClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.red,
-              child: Center(child: Text("OvalRightBorderClipper()")),
             ),
-          ),
-          const SizedBox(height: 10.0),
-          ClipPath(
-            clipper: SideCutClipper(),
-            child: Container(
-              height: 200,
-              color: Colors.pink,
-              //play with scals to get more clear versions
-              child: Center(child: Text("SideCutClipper()")),
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          ClipPath(
-            clipper: OvalLeftBorderClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.green,
-              child: Center(child: Text("OvalLeftBorderClipper()")),
-            ),
-          ),
-          const SizedBox(height: 10.0),
-          ClipPath(
-            clipper: ArcClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.pink,
-              child: Center(child: Text("ArcClipper()")),
-            ),
-          ),
-          ClipPath(
-            clipper: PointsClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.indigo,
-              child: Center(child: Text("PointsClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: TriangleClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.deepOrange,
-              child: Center(child: Text("TriangleClipper()")),
-            ),
-          ),
-          ClipPath(
-            clipper: MovieTicketClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.deepPurple,
-              child: Center(child: Text("MovieTicketClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: MovieTicketBothSidesClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.green,
-              child: Center(child: Text("MovieTicketBothSidesClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: MultipleRoundedCurveClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.pink,
-              child: Center(child: Text("MultipleRoundedCurveClipper()")),
-            ),
-          ),
-          SizedBox(height: 20.0),
-          ClipPath(
-            clipper: MultiplePointedEdgeClipper(),
-            child: Container(
-              height: 100,
-              color: Colors.green,
-              child: Center(child: Text("MultiplePointedEdgeClipper()")),
-            ),
-          ),
-          SizedBox(height: 20.0),
-          ClipPath(
-            clipper: OctagonalClipper(),
-            child: Container(
-              height: 220,
-              color: Colors.red,
-              child: Center(child: Text("OctagonalClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: HexagonalClipper(),
-            child: Container(
-              height: 220,
-              color: Colors.blueAccent,
-              child: Center(child: Text("HexagonalClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: HexagonalClipper(reverse: true),
-            child: Container(
-              height: 220,
-              color: Colors.orangeAccent,
-              child: Center(child: Text("HexagonalClipper(reverse: true)")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: ParallelogramClipper(),
-            child: Container(
-              height: 220,
-              color: Colors.pinkAccent,
-              child: Center(child: Text("ParallelogramClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: DiagonalPathClipperOne(),
-            child: Container(
-              height: 120,
-              color: Colors.red,
-              child: Center(child: Text("DiagonalPathClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: DiagonalPathClipperTwo(),
-            child: Container(
-              height: 120,
-              color: Colors.pink,
-              child: Center(child: Text("DiagonalPathClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: WaveClipperOne(),
-            child: Container(
-              height: 120,
-              color: Colors.deepPurple,
-              child: Center(child: Text("WaveClipperOne()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: WaveClipperOne(reverse: true),
-            child: Container(
-              height: 120,
-              color: Colors.deepPurple,
-              child: Center(child: Text("WaveClipperOne(reverse: true)")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: WaveClipperTwo(),
-            child: Container(
-              height: 120,
-              color: Colors.orange,
-              child: Center(child: Text("WaveClipperTwo()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: WaveClipperTwo(reverse: true),
-            child: Container(
-              height: 120,
-              color: Colors.orange,
-              child: Center(child: Text("WaveClipperTwo(reverse: true)")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: RoundedDiagonalPathClipper(),
-            child: Container(
-              height: 320,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                color: Colors.orange,
+            Padding(
+              padding: const EdgeInsets.only(left: 16,right: 16,top: 444,),
+              child: Column(
+                children: <Widget>[
+                  // Row(
+                  //   children: <Widget>[
+                  //     CircleAvatar(
+                  //       backgroundColor: Colors.white70,
+                  //       child: Text("fcdfcgfcdf"),
+                  //     ),
+                  //     SizedBox(width: 16.0),
+                  //     Expanded(
+                  //       child: Text("cdfgffdgcdfcgf dcfgfd dfgcfd",
+                  //         softWrap: true,
+                  //         // style: _questionStyle,
+                  //         ),
+                  //     ),
+                  //   ],
+                  // ),
+
+                  SizedBox(height: 20.0),
+                  Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        // ...options.map((option)=>
+                        
+                       TextField(
+  decoration: InputDecoration(
+    border: InputBorder.none,
+    hintText: 'Enter a search term'
+  ),
+)
+                        
+                        
+                        // ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)
+                        ),
+                        color: Theme.of(context).primaryColor,
+                        textColor: Colors.white,
+                        child: Text( "dgvrftvfg"),
+                        // onPressed: ,
+                      ),
+                    ),
+                  )
+                ],
               ),
-              child: Center(child: Text("RoundedDiagonalPathClipper()")),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ClipPath(
-            clipper: OvalTopBorderClipper(),
-            child: Container(
-              height: 120,
-              color: Colors.yellow,
-              child: Center(child: Text("OvalTopBorderClipper()")),
-            ),
-          ),
-          SizedBox(height: 10),
-          ClipPath(
-            clipper: ArrowClipper(70, 80, Edge.LEFT),
-            child: Container(
-              height: 120,
-              color: Colors.pink,
-              child: Center(child: Text("ArrowClipper()")),
-            ),
-          ),
-          SizedBox(height: 10),
-          ClipPath(
-            clipper: ArrowClipper(70, 80, Edge.RIGHT),
-            child: Container(
-              height: 120,
-              color: Colors.red,
-              child: Center(child: Text("ArrowClipper()")),
-            ),
-          ),
-          SizedBox(height: 10),
-          ClipPath(
-            clipper: ArrowClipper(20, 300, Edge.TOP),
-            child: Container(
-              height: 70,
-              width: 50,
-              color: Colors.blue,
-              child: Center(child: Text("ArrowClipper()")),
-            ),
-          ),
-          SizedBox(height: 10),
-          ClipPath(
-            clipper: ArrowClipper(70, 80, Edge.BOTTOM),
-            child: Container(
-              height: 120,
-              color: Colors.green,
-              child: Center(child: Text("ArrowClipper()")),
-            ),
-          ),
-          SizedBox(height: 10),
-          ClipPath(
-            clipper: StarClipper(8),
-            child: Container(
-              height: 450,
-              color: Colors.indigo,
-              child: Center(child: Text("StarClipper()")),
-            ),
-          ),
-          ClipPath(
-            clipper: MessageClipper(borderRadius: 16),
-            child: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                color: Colors.red,
-              ),
-              child: Center(child: Text("MessageClipper()")),
-            ),
-          ),
-        ],
-      ),
+            )
+          ],
+        ),
+
+      
+    
+          // ClipPath(
+          //   clipper: OvalBottomBorderClipper(),
+          //   child: Container(
+          //     height:MediaQuery.of(context).size.height/2,
+          //     color: Colors.blue,
+              
+          //     child: Center(child: Text("OvalBottomBorderClipper()")),
+          //   ),
+          // )
+
+
     );
   }
 }
@@ -370,7 +182,9 @@ class Login extends StatelessWidget {
 //           title: Text(widget.category.name),
 //           elevation: 0,
 //         ),
-//         body: Stack(
+//         body:
+        
+//          Stack(
 //           children: <Widget>[
 //             ClipPath(
 //               clipper: WaveClipperTwo(),
@@ -437,6 +251,8 @@ class Login extends StatelessWidget {
 //             )
 //           ],
 //         ),
+
+
 //       ),
 //     );
 //   }
--- lib/onboard.dart
+++ lib/onboard.dart
@@ -1,78 +1,78 @@
-import 'package:flutter/material.dart';
-import 'package:onboarding_anim/onboarding_anim.dart';
+// import 'package:flutter/material.dart';
+// import 'package:onboarding_anim/onboarding_anim.dart';
 
-class Onborad extends StatefulWidget {
-  @override
-  _OnboradState createState() => _OnboradState();
-}
+// class Onborad extends StatefulWidget {
+//   @override
+//   _OnboradState createState() => _OnboradState();
+// }
 
-class _OnboradState extends State<Onborad> {
-  List<PageModel> pages = <PageModel>[
-    PageModel(
-      title: "Relax",
-      body:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-      image: Image(
-          image: NetworkImage(
-              "https://i-love-png.com/images/kraken_body03_final_01.png")),
-    ),
-    PageModel(
-        title: "Flutter",
-        body:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-        image: Image(
-            image: NetworkImage(
-                "https://i-love-png.com/images/kraken_body01_final_01_1766.png"))),
-    PageModel(
-        title: "Is Awesome",
-        body:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-        image: Image(
-            image: NetworkImage(
-                "https://i-love-png.com/images/kraken_body03_final_01.png"))),
+// class _OnboradState extends State<Onborad> {
+//   List<PageModel> pages = <PageModel>[
+//     PageModel(
+//       title: "Relax",
+//       body:
+//           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+//       image: Image(
+//           image: NetworkImage(
+//               "https://i-love-png.com/images/kraken_body03_final_01.png")),
+//     ),
+//     PageModel(
+//         title: "Flutter",
+//         body:
+//             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+//         image: Image(
+//             image: NetworkImage(
+//                 "https://i-love-png.com/images/kraken_body01_final_01_1766.png"))),
+//     PageModel(
+//         title: "Is Awesome",
+//         body:
+//             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+//         image: Image(
+//             image: NetworkImage(
+//                 "https://i-love-png.com/images/kraken_body03_final_01.png"))),
 
 
                 
     
-    // PageModel(
-    //     title: "And Beautiful",
-    //     body:
-    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-    //     image: Image(
-    //         image: NetworkImage(
-    //             "https://i-love-png.com/images/kraken_body01_final_01_1766.png")))
+//     // PageModel(
+//     //     title: "And Beautiful",
+//     //     body:
+//     //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+//     //     image: Image(
+//     //         image: NetworkImage(
+//     //             "https://i-love-png.com/images/kraken_body01_final_01_1766.png")))
 
 
-  ];
+//   ];
 
-  @override
-  void initState() {
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: OnBoardingScreen(
-            onboardingDecoration: OnboardingDecoration(
-                aling: Alignment.topCenter,
-                child: Image(
-                  image:AssetImage("images/onboard_top.png"),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: OnBoardingScreen(
+//             onboardingDecoration: OnboardingDecoration(
+//                 aling: Alignment.topCenter,
+//                 child: Image(
+//                   image:AssetImage("images/onboard_top.png"),
                   
-                  // NetworkImage(
-                  //     "https://www.themexpert.com/images/easyblog_articles/531/b2ap3_large_Mountains.PNG"),
+//                   // NetworkImage(
+//                   //     "https://www.themexpert.com/images/easyblog_articles/531/b2ap3_large_Mountains.PNG"),
 
 
-                  width: MediaQuery.of(context).size.width,
-                )),
-            pages: pages,
-            showSkipButton: false,
-            onDone: () => print("Done"),
-            // onSkip: () => print("Skip"),
+//                   width: MediaQuery.of(context).size.width,
+//                 )),
+//             pages: pages,
+//             showSkipButton: false,
+//             onDone: () => print("Done"),
+//             // onSkip: () => print("Skip"),
             
-            // dotsType: OnBoardingScreen.dotsLinearProgress,
+//             // dotsType: OnBoardingScreen.dotsLinearProgress,
 
-            indicatorDecoration: IndicatorDecoration(
-                active: Colors.deepOrange, inactive: Colors.deepOrange[200])));
-  }
-}
+//             indicatorDecoration: IndicatorDecoration(
+//                 active: Colors.deepOrange, inactive: Colors.deepOrange[200])));
+//   }
+// }
